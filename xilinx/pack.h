562a5947a0f4c2d9de00984f094866a3a42cdba0
zhaolin <2766191688@qq.com>
2024-08-13 09:55:00 +0800

    添加IOBUFDS_DIFF_OUT、IOBUFDS_DIFF_OUT_INTERMDISAABLE、IOBUFDS_INTERMDISABLE，修复IBUFDS_DIFF_OUT相关原语差分输入连线错误问题

--- xilinx/pack.h
+++ xilinx/pack.h
@@ -227,6 +227,7 @@ struct XC7Packer : public XilinxPacker
     std::string get_odelay_site(const std::string &io_bel);
     std::string get_idelay_site(const std::string &io_bel);
 
+    void update_usr_ibufdisable(Context *ctx,CellInfo *inbuf);
     // Call before packing constants
     void prepare_iologic();
 

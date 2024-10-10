e19145f23b1f174282f01702925b36693bf166f2
lehaifeng <lehaifeng000@163.com>
2024-10-10 10:19:00 +0800

    同步github关于place的修改，修改ci artifact过期时间为2个月

--- common/placer_heap.cc
+++ common/placer_heap.cc
@@ -886,7 +886,7 @@ class HeAPPlacer
             total_iters_noreset++;
             if (total_iters > int(solve_cells.size())) {
                 total_iters = 0;
-                ripup_radius = std::max(std::max(max_x, max_y), ripup_radius * 2);
+                ripup_radius = std::min(std::max(max_x, max_y), ripup_radius * 2);
             }
 
             if (total_iters_noreset > std::max(5000, 8 * int(ctx->cells.size()))) {  // ?? why 5000? why 8 * num of cells?

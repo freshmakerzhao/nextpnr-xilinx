580910fce7e172e43220da920ec6601461fcca6c
liwenhao <liwenhao@zkxctech.com>
2025-03-06 11:06:04 +0800

    移动json.hpp和添加bel功耗获取函数

--- common/nextpnr.h
+++ common/nextpnr.h
@@ -949,6 +949,7 @@ class PowerResult {
 
         float GetStaticPowerResult() { return static_power_; }
         float GetJunctionTemp() { return junction_temp_; }
+        float GetResourcePower(IdString resource_name);
 };
 
 

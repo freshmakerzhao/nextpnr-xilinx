7c8becaea15fe6e82aabc95023a25201a20d9d03
ZhaoShuai <dsupermaker@163.com>
2025-04-24 15:41:52 +0800

    完善跨平台

--- config_scripts/config.cmake
+++ config_scripts/config.cmake
@@ -1,10 +1,19 @@
 # config.cmake
 
 # 设置 vcpkg 路径
-set(VCPKG_PATH "E:/workspace/vcpkg" CACHE STRING "Path to vcpkg installation")
+set(VCPKG_PATH "/opt/vcpkg_root/vcpkg" CACHE STRING "Path to vcpkg installation")
 set(CMAKE_TOOLCHAIN_FILE "${VCPKG_PATH}/scripts/buildsystems/vcpkg.cmake" CACHE STRING "Vcpkg toolchain file")
-set(CMAKE_PREFIX_PATH "${VCPKG_PATH}/installed/x64-windows;${CMAKE_PREFIX_PATH}")
+if(WIN32)
+    set(VCPKG_TARGET_TRIPLET "x64-windows")
+elseif(APPLE)
+    set(VCPKG_TARGET_TRIPLET "x64-osx")
+elseif(UNIX)
+    set(VCPKG_TARGET_TRIPLET "x64-linux")
+else()
+    message(FATAL_ERROR "Unsupported platform.")
+endif()
+
+set(CMAKE_PREFIX_PATH "${VCPKG_PATH}/installed/${VCPKG_TARGET_TRIPLET};${CMAKE_PREFIX_PATH}")
 
 # 设置压缩解压缩密码
 set(PRJ_PASSWORD "lwh123456" CACHE STRING "Set 7zip password")
-
